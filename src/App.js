40f59ce8dd05720d9b15986ed048495c765adfb5
moonstone <Moonseok1126@gmail.com>
2023-07-13 19:34:58 +0900

    test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://naver.com"
+          href="hhttps://react.dev/"
           target="_blank"
           rel="noopener noreferrer"
         >
