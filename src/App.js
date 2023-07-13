395e7fb8c8ebe45507fedc5aa3a1a494a56ddcd6
moonstone <Moonseok1126@gmail.com>
2023-07-13 19:28:27 +0900

    CI/CD test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://naver.com"
           target="_blank"
           rel="noopener noreferrer"
         >
