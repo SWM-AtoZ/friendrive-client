3db8d400edf76969d66611a84655636ef55af1de
sang-il Yoon <yunsangil7749@gmail.com>
2023-07-31 12:45:29 +0900

    Add Bottom Navbar

--- src/componenets/nav/Nav.js
+++ src/componenets/nav/Nav.js
@@ -1,36 +1,52 @@
 import style from "./nav.module.css"
 import { Link,useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import check from './check-list.png';
+import home from './home.png';
+import user from './user.png';
+
 const Nav = () => { 
     const [cookies,setCookie,removeCookie] = useCookies(['token']);
-
     const navigate = useNavigate();
-    const isLogin = (e) => {
-        const value = e.target.value;
+   
+    const goToSupplyment = () => {
         if(cookies.token){
-            if(value==='supplyment'){
                 navigate('/supplyment');
-            }
-            else if(value === 'user'){
-                navigate('./user');
-            }
         }
         else{
             alert('로그인이 필요한 서비스입니다.');
             navigate('/login');
         }
     }
-    return (<ul className={style.nav}>
-        <li className={style.link}>
-        <button value='supplyment' onClick={isLogin}>보충과목 페이지로</button>
-        </li>
-        <li className={style.link}>
-        <Link to={'/'}>홈페이지로</Link>
-        </li>
-        <li className={style.link}>
-        <button value='user' onClick={isLogin}>유저페이지로</button>
-        </li>
-    </ul>
+    const goToUser = () => {
+        if(cookies.token){
+            navigate('./user');
+        }
+        else{
+            alert('로그인이 필요한 서비스입니다.');
+            navigate('/login');
+        }
+    }
+    const goHome=()=>{
+        navigate('/');
+    }
+    return (
+    <div className={style.nav}>
+        <div onClick={goHome} className={style.link} >
+        <img src={home} className={style.img}/>
+        <div className={style.img_description}>홈</div>
+        </div>
+        
+        <div className={style.link} onClick={goToSupplyment}>
+        <img src={check} className={style.img}/>
+        <div className={style.img_description}>보충 과목</div>
+        </div>
+
+        <div className={style.link} onClick={goToUser}>
+        <img src={user} className={style.img}/>
+        <div className={style.img_description}>내 정보</div>
+        </div>
+    </div>
     )
 };
 
