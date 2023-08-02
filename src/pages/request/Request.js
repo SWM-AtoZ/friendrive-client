c7753c3dc0e3d1b561c902b42a75b3c3f3a18e62
sang-il Yoon <yunsangil7749@gmail.com>
2023-08-02 15:20:01 +0900

    Modify Share

--- src/pages/request/Request.js
+++ src/pages/request/Request.js
@@ -10,15 +10,9 @@ const Request = () => {
 const curriculum = useOutletContext().curriculum.curriculum;
 const checkRef = useRef([]);
 const [cookies,,] = useCookies([]);
-const [sharebtn, setSharebtn] = useState('true');
 
 //섹션의 수만큼의 크기를 가진 배열을 생성 추후에 해당 배열의 true 섹션만 쿼리스트링으로 표기
 const itemCheck = new Array(curriculum.length).fill(false);
-useEffect(()=>{
-    if(!navigator.canShare){
-        setSharebtn(false)
-    }
-},[])
 
 const clickbox = (e) =>{ //섹션박스 누르면 체크상태 변경, 각 섹션의 체크상태 itemCheck에 표시. 
     var click_section;        
@@ -66,7 +60,17 @@ const ShareTeacher = (checkArr) => {
     //티처토큰과, 체크된 섹션의 숫자 구해서 쿼리스트링에 담아주기.
     const teachertoken=cookies.teacherToken;
     const section =getSection(checkArr);
-    const url = `https://friendrive.net/teacher?section=${section}&teachertoken=${teachertoken}`
+    const url = `https://friendrive.net/teacher?section=${section}&teachertoken=${teachertoken}`;
+
+    const handleCopyClipBoard = async (uri) => {
+        try {
+          await navigator.clipboard.writeText(uri);
+          alert('클립보드에 링크가 복사되었습니다.');
+        } catch (e) {
+          alert('복사에 실패하였습니다');
+        }
+    };
+
     if (navigator.share) {
         navigator.share({
             title: '기록하며 성장하기',
@@ -74,7 +78,7 @@ const ShareTeacher = (checkArr) => {
             url: url,
         });
     }else{
-        alert("공유하기가 지원되지 않는 환경 입니다.");
+       handleCopyClipBoard(url);
     }
   }
 
@@ -90,9 +94,7 @@ const ShareTeacher = (checkArr) => {
            </div> 
         ))}
         </div>    
-        {sharebtn?(<button className={style.share_button} onClick={()=>{ShareTeacher(itemCheck)}}>공유하기</button>):
-        (<CopyToClipboard text={'zzzzz'} onCopy={() => alert("클립보드에 복사되었습니다.")}>
-        <button className={style.share_button}> 공유하기</button></CopyToClipboard>)}
+        <button className={style.share_button} onClick={()=>{ShareTeacher(itemCheck)}}>공유하기</button>
     </div>)
 }
 
