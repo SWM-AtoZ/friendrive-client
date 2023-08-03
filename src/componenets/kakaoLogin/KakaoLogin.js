f1a4895d8d8174a4a1e2a5a41f39a87e8b5cf905
sang-il Yoon <yunsangil7749@gmail.com>
2023-08-03 19:52:39 +0900

    Modify Login

--- src/componenets/kakaoLogin/KakaoLogin.js
+++ src/componenets/kakaoLogin/KakaoLogin.js
@@ -1,16 +1,15 @@
+import kakoLogin from './kakaoLogin.png';
+import style from './kakaologin.module.css';
+
 const KakaoLogin = () => {
     const CLIENT_ID = 'a7a85bcf9e7af40d7002b8fe4a670851';
     const REDIRECT_URI = 'https://friendrive.net/user';
     const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`
 
     return(
-        <button
-            width="255"
-            height="35"
-            style={{backgroundColor:'yellow'}}
-            onClick={() => window.location.href = kakaoURL}
-        >카카오 로그인
-        </button>
+        <div onClick={() => window.location.href = kakaoURL}>
+            <img className={style.login_btn} src={kakoLogin}/>
+        </div>
     )
 }
 
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,7 +3,12 @@ import KakaoLogin from '../../componenets/kakaoLogin/KakaoLogin';
 
 const Login = () => {
     return(<div className={style.login_button_container}>
+        <div className={style.login_text}>
+            <div>Friendrive 로그인</div>
+        </div>
+        <div className={style.login_innerbox}>
         <KakaoLogin/>
+        </div>
     </div>
         
     )
