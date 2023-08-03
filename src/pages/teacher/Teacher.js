11b1f4a9b8d5653b32df1df76663ba941366feae
sang-il Yoon <yunsangil7749@gmail.com>
2023-08-03 21:34:05 +0900

    Modify Teacher

--- src/pages/teacher/Teacher.js
+++ src/pages/teacher/Teacher.js
@@ -295,9 +295,7 @@ const Teacher = () => {
     // 선생님 페이지 get api
     const loadTeacherdata = async() =>{
         teacherToken = searchParams.get("teachertoken");
-        await setCookie('teacherToken',teacherToken,expires);
-        console.log(cookies.teacherToken);
-        await axios.get(`https://41icjhls1i.execute-api.ap-northeast-2.amazonaws.com/dev/teacher?token=${cookies.teacherToken}`)
+        await axios.get(`https://41icjhls1i.execute-api.ap-northeast-2.amazonaws.com/dev/teacher?token=${teacherToken}`)
         .then(function (response) {
             console.log(response.data);
             setTeacherPageData(response.data);
