abc8d6b8d324f1cbac36a781c0433b5a38a3eab8
sang-il Yoon <yunsangil7749@gmail.com>
2023-08-28 13:32:56 +0900

    test Curriculum Btn

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -212,8 +212,7 @@ function Home() {
         </div>
       </header>
       <article className={style.home_article}>
-      <button ref={Curriculum_btnRef} onTouchStart={()=>{Curriculum_btnRef.current.style.backgroundColor='rgba(0, 0, 0, 0.05)'}} onTouchEnd={()=>{Curriculum_btnRef.current.style.backgroundColor='#F8FBFC'; 
-      goToCurriculum();}} onClick={()=>{if(!isMobile){goToCurriculum()}}} className={style.curriculum_btn}>
+      <button ref={Curriculum_btnRef} onClick={()=>{goToCurriculum()}} className={style.curriculum_btn}>
         <div className={style.login_activation}>
           <div className={style.login_textArea}>
             <div>
