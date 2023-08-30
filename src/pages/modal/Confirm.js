f2284dc21b897ae0db17f00c09a48a6f6d7bd26d
sang-il Yoon <yunsangil7749@gmail.com>
2023-08-30 21:21:09 +0900

    Edit Confirm, Alert -로그아웃 confirm창 추가

--- src/pages/modal/Confirm.js
+++ src/pages/modal/Confirm.js
@@ -1,15 +1,29 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import style from './confirm.module.css';
+import { useCookies } from 'react-cookie';
+import { useOutletContext } from 'react-router-dom';
 
 const Confirm = () =>{
 const navigate = useNavigate();
 const location = useLocation();
 const title = location.state.message_title;
 const description = location.state.message_description;
+const Islogout = location.state.Islogout;
+
+const [cookies, setCookie, removeCookie] = useCookies(['']);
+const {setISLogin} = useOutletContext();
 const ClickOk = () =>{
-        navigate('/login',{
-            replace:true
-        });
+        if(!Islogout){
+            navigate('/login',{
+                replace:true
+            });
+        }
+        else{
+          removeCookie('token'); 
+          removeCookie('teacherToken');
+          setISLogin(false);
+          navigate(-1);
+        }
 }
 const ClicikCancle = () =>{
     navigate(-1);
