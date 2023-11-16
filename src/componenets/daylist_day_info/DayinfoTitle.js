54167430ff700ad99d422fe8878da5661cacba03
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-16 15:17:39 +0900

    refactor: 각 데이의 아이템 항목 갯수 수정

--- src/componenets/daylist_day_info/DayinfoTitle.js
+++ src/componenets/daylist_day_info/DayinfoTitle.js
@@ -1,97 +1,144 @@
-import style from './dayinfotitle.module.css';
-import { useEffect, useRef, useState } from 'react';
-import { Cookies } from 'react-cookie';
-import { isMobile } from 'react-device-detect';
-import { useNavigate } from 'react-router-dom';
+import style from "./dayinfotitle.module.css";
+import { useEffect, useRef, useState } from "react";
+import { Cookies } from "react-cookie";
+import { isMobile } from "react-device-detect";
+import { useNavigate } from "react-router-dom";
 
-const DayinfoTitle = ({day, title, dayprocess, confirm, memosLength}) => {
-    const DaysItemsTotalNumb = [7,8,4,5,4];
-    const TotalNumb = DaysItemsTotalNumb[day-1];
-    const [percentage, setPercentage] = useState('');
-    const navigate = useNavigate()
-    const cookies = new Cookies();
-    const circleRef = useRef();
+const DayinfoTitle = ({ day, title, dayprocess, confirm, memosLength }) => {
+  const DaysItemsTotalNumb = [7, 8, 4, 5, 2];
+  const TotalNumb = DaysItemsTotalNumb[day - 1];
+  const [percentage, setPercentage] = useState("");
+  const navigate = useNavigate();
+  const cookies = new Cookies();
+  const circleRef = useRef();
 
-    const goToWrite = () =>{
-        if(cookies.get('token')){
-            navigate('/writing',{
-                state : {
-                day : day
-            }});
+  const goToWrite = () => {
+    if (cookies.get("token")) {
+      navigate("/writing", {
+        state: {
+          day: day,
+        },
+      });
+    } else {
+      if (!isMobile) {
+        const loginConfirm = window.confirm(
+          "로그인이 필요한 서비스입니다, 로그인 하시겠습니까?"
+        );
+        if (loginConfirm) {
+          navigate("/login");
         }
-        else{
-            if(!isMobile){
-                const loginConfirm = window.confirm('로그인이 필요한 서비스입니다, 로그인 하시겠습니까?');
-                if(loginConfirm){
-                    navigate('/login');
-                }
-            }
-            else{
-                confirm();
-            }
-        }
+      } else {
+        confirm();
+      }
     }
+  };
 
-    useEffect(()=>{
-        //체크 목록 불러와서 몇개 체크되어있는지 확인하기.
+  useEffect(() => {
+    //체크 목록 불러와서 몇개 체크되어있는지 확인하기.
 
-        //로그인 유무에 따라 퍼센테이지 다르게 그려주기
-       const outline = circleRef.current.getTotalLength();
-       circleRef.current.style.strokeDasharray = outline;
-    
-       if(cookies.get('token')){
-        // 로그인이 된 경우
-        circleRef.current.style.strokeDashoffset = outline * (1 - Math.floor((dayprocess/TotalNumb) * 100) / 100);
-        const tempPercent = Math.floor((dayprocess/TotalNumb) * 100);
-        setPercentage(tempPercent);
-        if(tempPercent<=15&&tempPercent>0){
-            circleRef.current.style.stroke = "#FF0000";
-        }
-        else if(tempPercent>15 && tempPercent<100 ){
-            circleRef.current.style.stroke = "#FFE300";
-        }
-        else if(tempPercent===100){
-            circleRef.current.style.stroke = "#389300";
-        }
-       }
-       else{
-        // 로그인이 되지 않은 경우
-        circleRef.current.style.strokeDashoffset = outline;
-        setPercentage(0);
-       }
-    },[dayprocess])
+    //로그인 유무에 따라 퍼센테이지 다르게 그려주기
+    const outline = circleRef.current.getTotalLength();
+    circleRef.current.style.strokeDasharray = outline;
 
-    return(
-        <div className={style.curriculum_list}>
-                    <div className={style.curriculum_progress}>
-                    {/* dayprocess 의 값에 따라 진행율 적용하여 보여줌 */}
-                    <div className={style.circle_box}> 
-                    <svg className={style.circle_inner_box}  width="100%" height="100%" viewBox="0 0 200 200" fill="none">
-                        <circle  cx="100" cy="100" r="88" fill="none" stroke="#BDBDBD" stroke-width="12" />
-                        <circle className={style.circle} ref={circleRef} cx="100" cy="100" r="88" fill="none" stroke-width="12" />
-                    </svg>
-                    <div>
-                        {percentage?percentage:0}%
-                    </div>
-                    </div>
-                    <div className={style.title}>
-                    <div>Day {day}</div>
-                    <div>{title}</div>
-                    </div>
-                    </div>
-                    <div>
-                    {memosLength>0&&<button onClick={goToWrite}>
-                         <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 32 32" fill="none">
-                             <path fill-rule="evenodd" clip-rule="evenodd" d="M16 4C22.6274 4 28 9.37258 28 16C28 22.6274 22.6274 28 16 28C9.37258 28 4 22.6274 4 16C4 9.37258 9.37258 4 16 4Z" stroke="black" stroke-opacity="0.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                             <path d="M20 16.0001H12" stroke="black" stroke-opacity="0.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                             <path d="M16.0001 12V20" stroke="black" stroke-opacity="0.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </button>}
-                    </div>
-                
+    if (cookies.get("token")) {
+      // 로그인이 된 경우
+      circleRef.current.style.strokeDashoffset =
+        outline * (1 - Math.floor((dayprocess / TotalNumb) * 100) / 100);
+      const tempPercent = Math.floor((dayprocess / TotalNumb) * 100);
+      setPercentage(tempPercent);
+      if (tempPercent <= 15 && tempPercent > 0) {
+        circleRef.current.style.stroke = "#FF0000";
+      } else if (tempPercent > 15 && tempPercent < 100) {
+        circleRef.current.style.stroke = "#FFE300";
+      } else if (tempPercent === 100) {
+        circleRef.current.style.stroke = "#389300";
+      }
+    } else {
+      // 로그인이 되지 않은 경우
+      circleRef.current.style.strokeDashoffset = outline;
+      setPercentage(0);
+    }
+  }, [dayprocess]);
+
+  return (
+    <div className={style.curriculum_list}>
+      <div className={style.curriculum_progress}>
+        {/* dayprocess 의 값에 따라 진행율 적용하여 보여줌 */}
+        <div className={style.circle_box}>
+          <svg
+            className={style.circle_inner_box}
+            width="100%"
+            height="100%"
+            viewBox="0 0 200 200"
+            fill="none"
+          >
+            <circle
+              cx="100"
+              cy="100"
+              r="88"
+              fill="none"
+              stroke="#BDBDBD"
+              stroke-width="12"
+            />
+            <circle
+              className={style.circle}
+              ref={circleRef}
+              cx="100"
+              cy="100"
+              r="88"
+              fill="none"
+              stroke-width="12"
+            />
+          </svg>
+          <div>{percentage ? percentage : 0}%</div>
         </div>
-    )
-}
-
+        <div className={style.title}>
+          <div>Day {day}</div>
+          <div>{title}</div>
+        </div>
+      </div>
+      <div>
+        {memosLength > 0 && (
+          <button onClick={goToWrite}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.5rem"
+              height="1.5rem"
+              viewBox="0 0 32 32"
+              fill="none"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M16 4C22.6274 4 28 9.37258 28 16C28 22.6274 22.6274 28 16 28C9.37258 28 4 22.6274 4 16C4 9.37258 9.37258 4 16 4Z"
+                stroke="black"
+                stroke-opacity="0.5"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M20 16.0001H12"
+                stroke="black"
+                stroke-opacity="0.5"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M16.0001 12V20"
+                stroke="black"
+                stroke-opacity="0.5"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default DayinfoTitle;
--- src/pages/curriculum/Curriculum.js
+++ src/pages/curriculum/Curriculum.js
@@ -1,67 +1,79 @@
-import style from './curriculum.module.css';
-import TopNavi from '../../componenets/topNavi/TopNavi';
-import CurriculumList from '../../componenets/curriculumList/CurriculumList';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useCookies } from 'react-cookie';
+import style from "./curriculum.module.css";
+import TopNavi from "../../componenets/topNavi/TopNavi";
+import CurriculumList from "../../componenets/curriculumList/CurriculumList";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
 
 const Curriculum = () => {
-    const curriculum = ['넓은 공간에서 기본 조작 배우기','넓은 도로에서 기본 주행 배우기','주차 마스터하기','큰 도로에서 차선 변경 익히기','주행 정복하기']
-    const [day1, setDay1] = useState(0);
-    const [day2, setDay2] = useState(0);
-    const [day3, setDay3] = useState(0);
-    const [day4, setDay4] = useState(0);
-    const [day5, setDay5] = useState(0);
-    const setCheckedItemsNumb = [setDay1,setDay2,setDay3,setDay4,setDay5];
-    const CheckedItemsNumb = [day1, day2, day3, day4, day5];
-    const [cookies,,] = useCookies(['token']);
-    const test = new Array(5).fill(0);
+  const curriculum = [
+    "넓은 공간에서 기본 조작 배우기",
+    "넓은 도로에서 기본 주행 배우기",
+    "주차 마스터하기",
+    "큰 도로에서 차선 변경 익히기",
+    "주행 정복하기",
+  ];
+  const [day1, setDay1] = useState(0);
+  const [day2, setDay2] = useState(0);
+  const [day3, setDay3] = useState(0);
+  const [day4, setDay4] = useState(0);
+  const [day5, setDay5] = useState(0);
+  const setCheckedItemsNumb = [setDay1, setDay2, setDay3, setDay4, setDay5];
+  const CheckedItemsNumb = [day1, day2, day3, day4, day5];
+  const [cookies, ,] = useCookies(["token"]);
+  const test = new Array(5).fill(0);
 
-    useEffect( ()=>{
-            if(cookies.token){
-                axios.get('https://api.friendrive.net/curriculum/checked',{
-                    headers : {
-                        Authorization: `Bearer ${cookies.token}`
-                    }
-                })
-                .then(response =>{
-                    var TempCheckedItem  = response.data.checkedItem;
-                    //각 데이마다 몇개의 아이템이 체크되어있는지 확인.
-                    for(var i=0; i<setCheckedItemsNumb.length; i++){
-                        var day = i+1;
-                        for(var j=0; j<TempCheckedItem.length; j++){
-                            if(TempCheckedItem[j].includes(`d${day}`)){
-                                setCheckedItemsNumb[i](prev=>prev+1);
-                            }
-                        }
-                    }
-                })
-                .catch(response => {
-                    console.log(response);
-                })
+  useEffect(() => {
+    if (cookies.token) {
+      axios
+        .get("https://api.friendrive.net/curriculum/checked", {
+          headers: {
+            Authorization: `Bearer ${cookies.token}`,
+          },
+        })
+        .then((response) => {
+          var TempCheckedItem = response.data.checkedItem;
+          console.log(TempCheckedItem);
+          //각 데이마다 몇개의 아이템이 체크되어있는지 확인.
+          for (var i = 0; i < setCheckedItemsNumb.length; i++) {
+            var day = i + 1;
+            for (var j = 0; j < TempCheckedItem.length; j++) {
+              if (TempCheckedItem[j].includes(`d${day}`)) {
+                setCheckedItemsNumb[i]((prev) => prev + 1);
+              }
             }
+          }
+        })
+        .catch((response) => {
+          console.log(response);
+        });
+    }
 
-            return () =>{
-                //언마운트는 업데이트 전에 실행된다.
-                for(var i=0; i<setCheckedItemsNumb.length; i++){
-                    setCheckedItemsNumb[i](0);
-                }
-            }
-    },[])
+    return () => {
+      //언마운트는 업데이트 전에 실행된다.
+      for (var i = 0; i < setCheckedItemsNumb.length; i++) {
+        setCheckedItemsNumb[i](0);
+      }
+    };
+  }, []);
 
-    return(
-        <section className={style.curruculumSection}>
-        <TopNavi title={'커리큘럼'}/>
-        {/* 데이 리스트마다 프로그레스 바 추가., 각 curriculumList 클릭하면 해당 Daylist로 이동. */}
-        <div className={style.curriculum_list_container}>
-        <div  className={style.curriculum_list}>
-            {curriculum.map(
-                (item,idx)=>(<CurriculumList day={idx+1} title={item} progress={CheckedItemsNumb[idx]}/>)
-            )}
+  return (
+    <section className={style.curruculumSection}>
+      <TopNavi title={"커리큘럼"} />
+      {/* 데이 리스트마다 프로그레스 바 추가., 각 curriculumList 클릭하면 해당 Daylist로 이동. */}
+      <div className={style.curriculum_list_container}>
+        <div className={style.curriculum_list}>
+          {curriculum.map((item, idx) => (
+            <CurriculumList
+              day={idx + 1}
+              title={item}
+              progress={CheckedItemsNumb[idx]}
+            />
+          ))}
         </div>
-        </div>
-        </section>
-    )
-}
+      </div>
+    </section>
+  );
+};
 
 export default Curriculum;
